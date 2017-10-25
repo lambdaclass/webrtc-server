241385de3b5a1f5892d8c5f5e3849005850707e1
Facundo Olano <facundo.olano@gmail.com>
2017-10-25 13:03:15 -0300

    get auth function from configuration

--- README.md
+++ README.md
@@ -2,11 +2,32 @@
 
 An OTP application to make video calls using [WebRTC](https://webrtc.org/). It includes a STUN/TURN server using [processone/stun](https://github.com/processone/stun).
 
+## Configuration
+### authentication
+
+An authentication function needs to be provided to the app
+environment as which will be used to authenticate both the websocket
+connections to the signaling server and the turn connections.
+
+Example:
+
+``` erlang
+{auth_fun, {module, function}}
+```
+
+The function will ba called like `module:function(Username)`, and
+should return the expected password for the given Username. The
+password will compared to the one sent by the clients.
+
+## Websockets API for signaling
+
+TODO
+
 ## Run in development
 
     make dev
-    
-The app will run on `https://localhost:8443/:room` 
+
+The app will run on `https://localhost:8443/:room`
 
 ## Run in production
 
@@ -19,18 +40,18 @@ Update the configuration in `conf/sys.config`:
 * auth_realm: domain where it will be deployed, required for turn.
 * turn_ip: IP of the server where it will be deployed, required for turn.
 
+### Build a release
+
+    make release
+
+Unpack the generated tar and `bin/webrtc_erlang start`.
+
 ### Firewall setup for STUN/TURN
 
-``` 
+```
 iptables -A INPUT -p tcp --dport 3478 -j ACCEPT
 iptables -A INPUT -p udp --dport 3478 -j ACCEPT
 iptables -A INPUT -p tcp --dport 5349 -j ACCEPT
 iptables -A INPUT -p udp --dport 5349 -j ACCEPT
 iptables -A INPUT -p udp --dport 49152:65535 -j ACCEPT
-``` 
-
-### Build a release
-
-    make release
-    
-Unpack the generated tar and `bin/webrtc_erlang start`.
+```
--- src/webrtc_erlang_app.erl
+++ src/webrtc_erlang_app.erl
@@ -1,8 +1,3 @@
-%%%-------------------------------------------------------------------
-%% @doc webrtc_erlang public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(webrtc_erlang_app).
 
 -behaviour(application).
@@ -10,9 +5,7 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
-%%====================================================================
 %% API
-%%====================================================================
 
 start(_StartType, _StartArgs) ->
   Dispatch = cowboy_router:compile([
@@ -21,31 +14,34 @@ start(_StartType, _StartArgs) ->
                                            {"/css/[...]", cowboy_static, {priv_dir, webrtc_erlang, "/css"}},
                                            {"/websocket/:room", ws_handler, []},
                                            {'_', cowboy_static, {priv_file, webrtc_erlang, "/index.html"}}
-                                           ]}
+                                          ]}
                                    ]),
   {ok, _} = cowboy:start_tls(my_http_listener,
-                               [{port, config(port)},
-                                {certfile, config(certfile)},
-                                {keyfile, config(keyfile)}
-                               ],
-                               #{env => #{dispatch => Dispatch}}
-                              ),
+                             [{port, config(port)},
+                              {certfile, config(certfile)},
+                              {keyfile, config(keyfile)}
+                             ],
+                             #{env => #{dispatch => Dispatch}}
+                            ),
   syn:init(),
   stun_listener:add_listener(3478, udp, [{use_turn, true},
                                          {turn_ip, config(turn_ip)},
                                          {certfile, config(certfile)},
                                          {auth_type, user},
                                          {auth_realm, config(auth_realm)},
-                                         {auth_fun, fun(_User, _Realm) -> config(password) end}]),
+                                         {auth_fun, get_stun_auth_fun()}]),
   webrtc_erlang_sup:start_link().
 
-%%--------------------------------------------------------------------
 stop(_State) ->
-    ok.
+  ok.
 
-%%====================================================================
 %% Internal functions
-%%====================================================================
 config(Key) ->
   {ok, Value} = application:get_env(webrtc_erlang, Key),
   Value.
+
+get_stun_auth_fun() ->
+  {AuthMod, AuthFun} = config(auth_fun),
+  fun (User, _Realm) ->
+      AuthMod:AuthFun(User)
+  end.
