4928d7b4ef767396213b75faf4c5820cfacc890f
Facundo Olano <facundo.olano@gmail.com>
2017-12-27 10:29:48 -0300

    server events without data

--- src/webrtc_server.erl
+++ src/webrtc_server.erl
@@ -1,7 +1,9 @@
 -module(webrtc_server).
 
 -export([peers/1,
+         publish/2,
          publish/3,
+         send/2,
          send/3]).
 
 peers(Room) ->
@@ -10,6 +12,16 @@ peers(Room) ->
 publish(Room, Event, Data) ->
   Message = webrtc_utils:text_event(Event, Data),
   syn:publish(Room, Message).
+
+publish(Room, Event) ->
+  Message = webrtc_utils:text_event(Event),
+  syn:publish(Room, Message).
+
+send(Peer, Event) ->
+  Pid = syn:find_by_key(Peer),
+  Message = webrtc_utils:text_event(Event),
+  Pid ! Message,
+  ok.
 
 send(Peer, Event, Data) ->
   Pid = syn:find_by_key(Peer),
